9ec739c104a42c7fb0275720823e5b4946e8864a
aschausson <aschausson@gmail.com>
2018-02-01 19:29:14 +0000

    Add files via upload

--- juego.js
+++ juego.js
@@ -37,3 +37,28 @@ function bombardea(){
 function endGame(){
 
 }
+
+
+jQuery(window).bind('orientationchange', function(e) {
+    switch ( window.orientation ) {
+     case 0:
+       $('.turnDeviceNotification').css('display', 'none');
+       // The device is in portrait mode now
+     break;
+   
+     case 180:
+       $('.turnDeviceNotification').css('display', 'none');
+       // The device is in portrait mode now
+     break;
+   
+     case 90:
+       // The device is in landscape now
+       $('.turnDeviceNotification').css('display', 'block');
+     break;
+   
+     case -90:
+       // The device is in landscape now
+       $('.turnDeviceNotification').css('display', 'block');
+     break;
+    }
+   });
